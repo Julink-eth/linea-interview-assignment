aa5385ae172ef051a7352a1522df164f45be9082
julink-eth <julien.fontanel@consensys.net>
2024-06-17 12:01:33 +0200

    fix: ts errors

--- test/ClaimableAirdrop.ts
+++ test/ClaimableAirdrop.ts
@@ -6,19 +6,19 @@ describe("ClaimableAirdrop", function () {
   let claimableAirdrop;
   let addresses: (string | number)[][];
   let merkleTree: MerkleTree;
-  let addrs: [];
+  let signers;
 
   beforeEach(async () => {
     // Hardhat creates a 100 test accounts
-    addrs = await hre.ethers.getSigners();
+    signers = await hre.ethers.getSigners();
     addresses = [
-      [addrs[0].address, 100],
-      [addrs[1].address, 50],
-      [addrs[2].address, 20],
-      [addrs[3].address, 30],
-      [addrs[4].address, 80],
-      [addrs[5].address, 25],
-      [addrs[6].address, 15],
+      [signers[0].address, 100],
+      [signers[1].address, 50],
+      [signers[2].address, 20],
+      [signers[3].address, 30],
+      [signers[4].address, 80],
+      [signers[5].address, 25],
+      [signers[6].address, 15],
     ];
 
     merkleTree = new MerkleTree(addresses);
@@ -33,7 +33,7 @@ describe("ClaimableAirdrop", function () {
     for (let i = 0; i < addresses.length; i++) {
       const proof = merkleTree.getProof(i);
       const result = await claimableAirdrop
-        .connect(addrs[i])
+        .connect(signers[i])
         .canClaimTokens(proof, addresses[i][1]);
       expect(result).to.equal(true);
     }
@@ -42,7 +42,7 @@ describe("ClaimableAirdrop", function () {
   it("Should return false if claimant passes wrong proof index", async function () {
     const proof = merkleTree.getProof(3);
     const result = await claimableAirdrop
-      .connect(addrs[1])
+      .connect(signers[1])
       .canClaimTokens(proof, addresses[1][1]);
     expect(result).to.equal(false);
   });
@@ -50,7 +50,7 @@ describe("ClaimableAirdrop", function () {
   it("Should return false if claimant passes wrong amount", async function () {
     const proof = merkleTree.getProof(1);
     const result = await claimableAirdrop
-      .connect(addrs[1])
+      .connect(signers[1])
       .canClaimTokens(proof, 10000);
     expect(result).to.equal(false);
   });
